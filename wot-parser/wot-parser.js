d7f5801ae779a18274e18eccc64b48ad962e9d92
Schurtenberger Benjamin z0039cjn <benjamin.schurtenberger@siemens.com>
2017-03-07 09:36:09 +0100

    Added comments

--- wot-parser/wot-parser.js
+++ wot-parser/wot-parser.js
@@ -1,4 +1,8 @@
 module.exports = function(RED) {
+	/**
+	 * Represents a node of type 'WoT-Parser'.
+	 * @param {config} config - The node-configuration.
+	 */
     function WotParserNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -9,6 +13,13 @@ module.exports = function(RED) {
 			
 			http_get(config.url_host, config.url_port, config.url_path, http_get_callback);
 			
+			/**
+			 * Does an async http-request (GET-method)
+			 * @param {string} host - The host of the website requested.
+			 * @param {int} port - The port to use for the query (default HTTP is 80).
+			 * @param {string} path - The path where to search for data if it is not at the mainpage.
+			 * @param {function} callback - The callback-function which gets called when the request completed.
+			 */
 			function http_get(host, port, path, callback)
 			{
 				var http = require('http');
@@ -22,6 +33,10 @@ module.exports = function(RED) {
 				http.get(options, callback);
 			}
 			
+			/**
+			 * Handles the request's response
+			 * @param {http.IncomingMessage} response - The response from the http-request.
+			 */
 			function http_get_callback(response) {
 				response.on('data', (chunk) => {
 					try
